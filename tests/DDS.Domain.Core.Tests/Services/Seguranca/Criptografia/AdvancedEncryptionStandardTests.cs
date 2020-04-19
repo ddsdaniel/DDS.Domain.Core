c4798f25c094db6b764db8ab860dab3366c9ced5
Daniel <dds.daniel@gmail.com>
2020-04-19 09:11:57 -0300

    AdvancedEncryptionStandardTests

---
+++ tests/DDS.Domain.Core.Tests/Services/Seguranca/Criptografia/AdvancedEncryptionStandardTests.cs
@@ -0,0 +1,27 @@
+﻿using Bogus;
+using DDS.Domain.Core.Services.Seguranca.Criptografia;
+using System;
+using Xunit;
+
+namespace DDS.Domain.Core.Tests.Services.Seguranca.Criptografia
+{
+    public class AdvancedEncryptionStandardTests
+    {
+        [Fact]
+        public void CifrarDecifrar_DadosAleatorios_SucessoNaDecifragem()
+        {
+            //Arrange
+            var mensagem = new Faker("pt_BR").Person.UserName;
+            var chaveSecreta = Guid.NewGuid().ToString();
+            var algoritmo = new AdvancedEncryptionStandard();
+
+            //Act
+            var mensagemCifrada = algoritmo.Cifrar(mensagem, chaveSecreta);
+            var mensagemDecifrada = algoritmo.Decifrar(mensagemCifrada, chaveSecreta);
+
+            //Assert
+            Assert.NotEqual(mensagem, mensagemCifrada);
+            Assert.Equal(mensagem, mensagemDecifrada);
+        }
+    }
+}
