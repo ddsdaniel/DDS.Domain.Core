4f11f2cb103ce960a986afc9302bcec5b5c6c50a
Daniel <dds.daniel@gmail.com>
2020-08-30 09:35:04 -0300

    NotifiableExtensions

---
+++ src/DDS.Domain.Core/Extensions/NotifiableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Flunt.Notifications;
+
+namespace DDS.Domain.Core.Extensions
+{
+    public static class NotifiableExtensions
+    {
+
+        /// <summary>
+        /// Adiciona as notificações dos itens passados por parâmetros apenas se nenhum deles for nulo
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="items"></param>
+        public static void AddNotificationsIfNotNull(this Notifiable source, params Notifiable[] items)
+        {
+            foreach (var item in items)
+            {
+                if (item != null)
+                    source.AddNotifications(item);
+            }
+        }
+    }
+}
