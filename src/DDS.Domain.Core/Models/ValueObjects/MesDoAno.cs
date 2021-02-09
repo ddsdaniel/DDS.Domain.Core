fc32cfa4d82b618100113aaa925b71322425b16e
Daniel <dds.daniel@gmail.com>
2021-02-09 14:47:45 -0300

    antes de formatar

---
+++ src/DDS.Domain.Core/Models/ValueObjects/MesDoAno.cs
@@ -0,0 +1,22 @@
+﻿using DDS.Domain.Core.Abstractions.Models.ValueObjects;
+using Flunt.Validations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DDS.Domain.Core.Models.ValueObjects
+{
+    public class MesDoAno : ValueObject<MesDoAno>
+    {
+        public int Mes { get; private set; }
+
+        [ExcludeFromCodeCoverage] protected MesDoAno() { }
+
+        public MesDoAno(int mes)
+        {
+            AddNotifications(new Contract()
+                .IsBetween(mes, 1, 12, nameof(Mes), "Mês deve ser entre 1 e 12")
+                );
+
+            Mes = mes;
+        }
+    }
+}
---
+++ tests/DDS.Domain.Core.Mocks/Models/ValueObjects/MesDoAnoMock.cs
@@ -0,0 +1,25 @@
+﻿using Bogus;
+using DDS.Domain.Core.Models.ValueObjects;
+
+namespace DDS.Domain.Core.Mocks.Models.ValueObjects
+{
+    public static class MesDoAnoMock
+    {
+        public static MesDoAno ObterValido()
+        {
+            return new Faker<MesDoAno>()
+              .CustomInstantiator(e => new MesDoAno(e.Random.Int(1, 12)))
+              .Generate();
+        }
+
+        public static MesDoAno ObterInvalido()
+        {
+            return new MesDoAno(0);
+        }
+
+        public static MesDoAno ObterComMes(int mes)
+        {
+            return new MesDoAno(mes);
+        }
+    }
+}
